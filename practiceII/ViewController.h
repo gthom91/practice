a236d1b4e76b80d3a20526c95cc67e24186a0663
Greg Thomas <greg.thomas@nwktc.edu>
2013-05-03 08:42:20 -0600

    Initial Commit

---
+++ practiceII/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  practiceII
+//
+//  Created by Student on 5/3/13.
+//  Copyright (c) 2013 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
